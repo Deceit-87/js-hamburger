678a8ea993f5f74368ef6f7c67c59c479ec95160
Deceit-87 <manuelfreund318@gmail.com>
2022-02-08 17:20:19 +0100

    aggiunta classe oped ad <a>

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+
+
+// Aggiungere classe active al menu hamburger class fa-bars
+// oppure aggiungere classe open al link (no id no classe )
+
+// togliere la classe active con la x classe close 
+
+
+
+let openButton = document.querySelector('.header-right > a');
+let openButton = document.querySelector('.header-right > a');
+console.log(openButton);
+
+openButton.onclick =function() {
+    console.log('event')
+}
